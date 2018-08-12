8178b126271840747c6b144b6c447488d0f83829
Martin Wheeler <martin@martinwheeler.com.au>
2018-08-12 20:57:34 +1000

    Replace react app with create-react-app

--- app/frontend/src/index.js
+++ app/frontend/src/index.js
@@ -1,42 +1,8 @@
-import React, { Fragment } from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { applyMiddleware, createStore } from 'redux'
-import { createLogger } from 'redux-logger'
-import promiseMiddleware from 'redux-promise-middleware'
-import thunkMiddleware from 'redux-thunk'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-import HomePage from 'pages/home';
-import AboutPage from 'pages/about';
-
-const middleware = [
-  thunkMiddleware,
-  promiseMiddleware({
-    promiseTypeSuffixes: [ 'REQ', 'ACK', 'ERR' ]
-  })
-];
-
-if (process.env.NODE_ENV !== 'production') {
-  middleware.push(createLogger());
-}
-
-const store = createStore(
-  // reducer,
-  applyMiddleware(...middleware)
-);
-
-// store.dispatch(fetchAllDummyItems())
-// store.dispatch(fetchContainerId())
-
-render(
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <Route exact path="/" component={HomePage}/>
-        <Route path="/about" component={AboutPage}/>
-      </Fragment>
-    </Router>
-  </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
