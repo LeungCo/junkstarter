033e65ed8d624978b23149e655958df7cbeb89f2
Delon Leung <delonleung@gmail.com>
2018-10-21 21:11:24 +1000

    fix build errors

--- app/src/test/java/com/docker/junkstarter/test/integration/event/EventRepositoryIntegrationTest.java
+++ app/src/test/java/com/docker/junkstarter/test/integration/event/EventRepositoryIntegrationTest.java
@@ -27,7 +27,7 @@ import com.github.springtestdbunit.annotation.DatabaseSetup;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, DbUnitTestExecutionListener.class })
-@DatabaseSetup("events.xml")
+@DatabaseSetup("./events.xml")
 public class EventRepositoryIntegrationTest {
 
 	private static final UUID EVENT_ID1 = UUID.fromString("667fd724-2ac5-466b-ab85-948f42b0d372");
